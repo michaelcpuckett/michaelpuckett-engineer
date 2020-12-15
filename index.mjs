191d9e10ca20719f4c2cbedbf008733774ad8d28
Michael Puckett <michaelcpuckett@gmail.com>
2020-12-15 09:29:45 -0500

    componentize

--- index.mjs
+++ index.mjs
@@ -1,4 +1,5 @@
 import Vue from 'https://unpkg.com/vue@2.6.12/dist/vue.esm.browser.min.js'
+import analyticsScript from './analyticsScript.mjs'
 import {
   createElement,
   queryFor,
@@ -8,6 +9,16 @@ import {
   fetchTemplateFromScript,
   domContentLoadedPromise
 } from './util/index.js'
+
+const getConfig = async () => {
+  const scriptEl = queryFor('script[data-config]')
+  const templateEl = await fetchTemplateFromScript(scriptEl)
+  const jsonScriptEl = templateEl.content.firstElementChild
+  window.document.body.append(jsonScriptEl)
+  scriptEl.remove()
+  const jsonString = jsonScriptEl.innerHTML
+  return JSON.parse(jsonString.replace(/"@/g, '"'))
+}
 
 const registerComponents = async () => {
   const componentScriptEls = queryAll('script[data-component]')
@@ -35,18 +46,19 @@ const getHeadTemplate = async () => {
   return template
 }
 
-const renderHead = async data => {
+const renderHead = async config => {
+  const template = await getHeadTemplate()
   const el = createElement('div')
-  window.document.body.append(el)
+  window.document.body.prepend(el)
   
   new Vue({
     el,
   	data: () => ({
   		config: {
-  			...data
+  			...config
   		}
   	}), 
-    template: await getHeadTemplate(),
+    template,
     mounted() {
       ;[...this.$el.children].forEach(el => {
         window.document.head.append(el)
@@ -59,51 +71,85 @@ const renderHead = async data => {
   })
 }
 
-const getAppTemplate = async () => {
-  const el = queryFor('script[data-app]')
+const getBodyTemplate = async () => {
+  const el = queryFor('script[data-body]')
   const template = await fetchTemplateFromScript(el)
     .then(getTemplateContents)
   el.remove()
   return template
 }
 
-const renderApp = async data => {
+const renderBody = async config => {
+  const template = await getBodyTemplate()
   const el = createElement('div')
-  window.document.body.append(el)
+  window.document.body.prepend(el)
 
   return new Vue({
-    	el,
-    	data: () => ({
-    		config: {
-    			...data
-    		}
-    	}),
-      template: await getAppTemplate()
-    })
+    el,
+    data: () => ({
+      config: {
+        ...config
+      }
+    }),
+    template
+  })
 }
 
-domContentLoadedPromise.then(async () => {
-  const dataEl = queryFor('[type="application/ld+json"]')
-  const appData = JSON.parse(dataEl.innerHTML.replace(/\"@/g, '"'))
+const render = async config => {
+  await registerComponents()
+  await renderHead(config)
+  await renderBody(config)
+  return config
+}
 
-  await Promise.all([
-    registerComponents(),
-    renderApp(appData)
-  ])
-  
-  await renderHead(appData)
-
-  const stylesheetEl = queryFor('link[rel="stylesheet"]')
-  const style = await fetchTextContent(stylesheetEl.getAttribute('href'))
+const inlineStyles = async () => {
+  const styleSheetEl = queryFor('link[rel="stylesheet"]')
+  const style = await fetchTextContent(styleSheetEl.getAttribute('href'))
   const styleEl = createElement('style')
   styleEl.innerHTML = style
-  stylesheetEl.parentElement.replaceChild(styleEl, stylesheetEl)
+  styleSheetEl.parentElement.replaceChild(styleEl, styleSheetEl)
+}
 
+const prepareOutput = async ({
+  knowsLanguage: [{
+    alternateName: lang
+  }]
+}) => {
+  await inlineStyles()
   queryFor('script[type="module"]').remove()
-  console.log(`
+
+  return `
     <!doctype html>
-    <html lang="en">
-      ${window.document.documentElement.innerHTML}
+    <html lang="${lang}">
+      <head>
+        ${window.document.head.innerHTML}
+      </head>
+      <body>
+        ${window.document.body.innerHTML}
+        ${analyticsScript}
+      </body>
     </html>
-  `)
-})
+  `
+}
+
+domContentLoadedPromise
+  .then(getConfig)
+  .then(render)
+  .then(prepareOutput)
+  .then(html => {
+    const file = new File([ html ], 'index.html', {
+      type: 'text/html'
+    })
+    const url = window.URL.createObjectURL(file)
+    const downloadLinkEl = createElement('a')
+    downloadLinkEl.setAttribute('download', 'index.html')
+    downloadLinkEl.innerText = 'Download'
+    downloadLinkEl.setAttribute('href', url)
+    downloadLinkEl.style.position = 'fixed'
+    downloadLinkEl.style.top = '10px'
+    downloadLinkEl.style.right = '10px'
+    downloadLinkEl.style.backgroundColor = 'var(--swatch-text-color)'
+    downloadLinkEl.style.color = 'var(--swatch-background-color)'
+    downloadLinkEl.style.padding = '5px 10px'
+    window.document.body.appendChild(downloadLinkEl)
+  })
